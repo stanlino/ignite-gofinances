2e45c3c9482f276d1afe0f15278a0a717a531801
Stanley Filipe <stanley92filipe@gmail.com>
2022-02-17 16:35:19 -0300

    add SignInSocialButton, a SignIn screen component

---
+++ src/components/SignInSocialButton/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { GestureHandlerRootView, RectButtonProps } from 'react-native-gesture-handler'
+import { SvgProps } from 'react-native-svg'
+
+import { 
+  Button,
+  ImageContainer,
+  Text
+} from './styles'
+
+interface Props extends RectButtonProps {
+  title: string
+  svg: React.FC<SvgProps>
+}
+
+export function SignInSocialButton({
+  title,
+  svg: Svg,
+  ...rest
+} : Props) {
+  return (
+    <GestureHandlerRootView>
+      <Button {...rest}>
+        <ImageContainer>
+          <Svg />
+        </ImageContainer>
+
+        <Text>
+          {title}
+        </Text>
+      </Button>
+    </GestureHandlerRootView>
+  )
+}
